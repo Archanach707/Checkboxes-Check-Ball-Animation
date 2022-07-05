2abd84d9d03aa1d53561f5cb7db4bf9fa440c4b0
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2022-07-05 17:14:29 +0530

    make variables

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const toggles = document.querySelectorAll('toggle')
+const good = document.querySelectorAll('#good')
+const cheap = document.querySelectorAll('#cheap')
+const fast = document.querySelectorAll('#fast')
+
+
+
+
+
+
+
